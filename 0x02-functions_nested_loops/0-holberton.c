6712804484a5d5f51988e65246a06feec5c3efad
Chinemerem Afam-echi <ingeniouschinemerem@gmail.com>
2021-09-01 15:08:22 -0700

    nested loop task

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,15 +6,8 @@
   */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
+	_putchar('h');
 	_putchar('\n');
-	
-	return (0);
+
+	return 0
 }
