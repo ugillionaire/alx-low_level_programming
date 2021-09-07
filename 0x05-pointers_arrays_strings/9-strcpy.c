683026240f301668db119a00e889951cc4edc11f
Chinemerem Afam-echi <ingeniouschinemerem@gmail.com>
2021-09-07 03:44:30 -0700

    pointers_arrays

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -19,22 +19,4 @@ char *_strcpy(char *dest, char *src)
 	dest[i++] = '\0';
 
 	return (dest);
-}trcpy - Copy a string
-* @dest: Destination value
-* @src: Source value
-  *
-* Return: the pointer to dest
-  */
-char *_strcpy(char *dest, char *src)
-{
-  int i;
-
-  for (i = 0; src[i] != '\0'; i++)
-    {
-      dest[i] = src[i];
-    }
-
-  dest[i++] = '\0';
-
-  return (dest);
 }
